17da70ac2f75647da0977a871e8cfba5d6b58c0a
Jan Lafferton <jan.lafferton@vector.com>
2025-01-27 15:41:36 +0000

    Simple window

--- flake.nix
+++ flake.nix
@@ -9,15 +9,22 @@
       pkgs = nixpkgs.legacyPackages.${system};
     in
     {
-      devShells.${system}.default = pkgs.mkShell {
+      devShells.${system}.default = pkgs.mkShell rec {
         nativeBuildInputs = with pkgs; [
           cargo
           rustc
         ];
         buildInputs = with pkgs; [
-          wayland
-          vulkan-loader
+          libGL
+          libxkbcommon
+          xorg.libX11
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXrandr
+          xorg.libXinerama
         ];
+        RUST_LOG = "info";
+        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
       };
     };
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use winit::{
+    application::ApplicationHandler,
+    event::WindowEvent,
+    event_loop::{self, EventLoop},
+    window::{Window, WindowAttributes},
+};
+
+#[derive(Default)]
+struct App {
+    window: Option<Window>,
+}
+
+impl ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &event_loop::ActiveEventLoop) {
+        self.window = Some(
+            event_loop
+                .create_window(WindowAttributes::default())
+                .unwrap(),
+        );
+    }
+
+    fn window_event(
+        &mut self,
+        event_loop: &event_loop::ActiveEventLoop,
+        _window_id: winit::window::WindowId,
+        event: winit::event::WindowEvent,
+    ) {
+        match event {
+            WindowEvent::CloseRequested => event_loop.exit(),
+            _ => {}
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::try_init().unwrap();
+
+    let event_loop = EventLoop::new().unwrap();
+    event_loop.run_app(&mut App::default()).unwrap();
 }
