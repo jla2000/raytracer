c6165483e41e0ff4c1d68fb2e8856163308796f5
Jan Lafferton <jan.lafferton@vector.com>
2025-01-27 15:08:06 +0000

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          cargo
+          rustc
+        ];
+        buildInputs = with pkgs; [
+          wayland
+          vulkan-loader
+        ];
+      };
+    };
+}
