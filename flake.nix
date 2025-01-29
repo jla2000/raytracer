658d6c95b7f0f762b7e7889bbad91f573b131cc3
Jan Lafferton <jan.lafferton@vector.com>
2025-01-29 15:24:41 +0000

    Cross compilation devshell

--- flake.nix
+++ flake.nix
@@ -1,32 +1,51 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, rust-overlay }:
     let
       system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ (import rust-overlay) ];
+      };
     in
     {
-      devShells.${system}.default = pkgs.mkShell rec {
-        nativeBuildInputs = with pkgs; [
-          cargo
-          rustc
-        ];
-        buildInputs = with pkgs; [
-          vulkan-loader
-          libGL
-          libxkbcommon
-          wayland
-          xorg.libX11
-          xorg.libXcursor
-          xorg.libXi
-          xorg.libXrandr
-          xorg.libXinerama
-        ];
-        RUST_LOG = "info";
-        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
+      devShells.${system} = rec {
+        default = pkgs.mkShell rec {
+          nativeBuildInputs = with pkgs; [
+            rust-bin.stable.latest.minimal
+          ];
+          buildInputs = with pkgs; [
+            vulkan-loader
+            libGL
+            libxkbcommon
+            wayland
+            xorg.libX11
+            xorg.libXcursor
+            xorg.libXi
+            xorg.libXrandr
+            xorg.libXinerama
+          ];
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
+        };
+
+        windows = pkgs.mkShell rec {
+          nativeBuildInputs = with pkgs; [
+            (rust-bin.stable.latest.minimal.override {
+              targets = [
+                "x86_64-pc-windows-gnu"
+              ];
+            })
+            pkgsCross.mingwW64.buildPackages.gcc
+            pkgsCross.mingwW64.windows.pthreads
+          ];
+        };
       };
     };
 }
